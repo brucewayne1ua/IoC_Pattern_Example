d5b20bf521d94fe7e9e0f0ff8468fb2454a3f510
Rustam Ohli <adamsonrustam@gmail.com>
2024-07-11 13:26:15 +0300

    Initial commit

---
+++ src/main/Java/Main/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package Main;
+
+public class ClassicalMusic implements Music {
+	@Override
+	public String getSong() {
+		return "Hungarian Rhapsody";
+	}
+}
---
+++ src/main/Java/Main/Music.java
@@ -0,0 +1,5 @@
+package Main;
+
+public interface Music {
+	String getSong();
+}
